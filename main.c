862e8839f114b3a3c14bbcf78aac0060aaf692e8
mr-koc <kocali4244@gmail.com>
2025-12-24 14:45:04 +0300

    Add files via upload
    1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    int fact = 1;
+
+    printf("Faktoriyel istediginiz sayiyi giriniz: ");
+    scanf("%d", &n);
+
+    while (n !=0) {
+printf("%d\n", n);
+        fact = fact * n;
+
+        n--;
+    }
+    printf("%d\n", fact);
+    return 0;
+
+}
